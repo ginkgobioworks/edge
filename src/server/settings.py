b2be5069e973e6feb12f263da7b3f543e56b8552
Benjie Chen <benjie@ginkgobioworks.com>
2014-02-01 20:38:17 -0500

    example settings for mysql

--- src/server/settings.py
+++ src/server/settings.py
@@ -66,11 +66,22 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': { "init_command": "SET storage_engine=INNODB;" },
+        'HOST': '',
+        'PORT': '',
+        'NAME' : 'edge',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'ATOMIC_REQUESTS': True,
     }
 }
+DATABASES['default'] = DATABASES['sqlite']
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
