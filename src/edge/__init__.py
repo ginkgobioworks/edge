ebfa7cd62e7c8074c9e437dda6460cc96e38724e
Benjie Chen <benjie@ginkgobioworks.com>
2014-02-03 11:27:05 -0500

    setup SQLite to use FK=ON, JOURNAL_MODE=MEMORY

---
+++ src/edge/__init__.py
@@ -0,0 +1,10 @@
+from django.db.backends.signals import connection_created
+
+def setup_sqlite3(sender, connection, **kwargs):
+    """Enable integrity constraint with sqlite."""
+    if connection.vendor == 'sqlite':
+        cursor = connection.cursor()
+        cursor.execute('PRAGMA foreign_keys = ON;')
+        cursor.execute('PRAGMA journal_mode = MEMORY;')
+
+connection_created.connect(setup_sqlite3)
