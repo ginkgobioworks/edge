423523c3f14f80f7996b5b60f29664b6f157bea4
Benjie Chen <benjie@ginkgobioworks.com>
2014-08-26 16:18:28 -0400

    fix asset issues

--- setup.py
+++ setup.py
@@ -6,13 +6,6 @@ import os
 import sys
 import subprocess
 
-def bower():
-    if sys.argv[1] in ['develop', 'install']:
-        print 'Running bower update in %s' % os.getcwd()
-        subprocess.call(['bower', 'update'])
-
-bower()
-
 with open("requirements/core.txt", "r") as f:
     install_requires = [x.strip() for x in f.readlines() if not x.strip().startswith("#")]
 
