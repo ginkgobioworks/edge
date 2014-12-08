e6ea3c4d1c706f9e22fda931b3966fdbceb09341
Benjie Chen <benjie@ginkgobioworks.com>
2014-12-08 13:05:33 -0500

    Update UI to display operations from parent genome

--- src/edge/assets.py
+++ src/edge/assets.py
@@ -3,6 +3,9 @@ from django_assets import Bundle, register
 js = Bundle('edge/src/js/*.js', filters='jsmin', output='edge/static/edge/edge.min.js')
 register('edge_js', js)
 
+js = Bundle('edge/src/js/*.js', output='edge/static/edge/edge.js')
+register('edge', js)
+
 css = Bundle('edge/src/css/app.css', output='edge/static/edge/edge.css')
 register('edge_css', css)
 
