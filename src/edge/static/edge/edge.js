91badbb727fcf451eb98bd4b76be1a222616d920
Benjie Chen <benjie@ginkgobioworks.com>
2015-03-04 10:31:50 -0500

    Add messages about waiting for server to respond to blast request

--- src/edge/static/edge/edge.js
+++ src/edge/static/edge/edge.js
@@ -45,12 +45,17 @@ function GenomeBlastController($scope, $routeParams, $http) {
     $scope.error = undefined;
 
     $scope.Blast = function(query, program) {
+        $scope.waiting = true;
         var data = JSON.stringify({query: query, program: program});
         $http.post('/edge/genomes/'+$scope.genomeId+'/blast/', data)
              .success(function(results) {
+                 $scope.waiting = false;
                  $scope.results = results;
              })
-             .error(function(data, status, headers, config) { $scope.errors = data; });
+             .error(function(data, status, headers, config) {
+                 $scope.waiting = false;
+                 $scope.errors = data;
+             });
     };
 
     $http.get('/edge/genomes/'+$scope.genomeId+'/').success(function(data) { $scope.genome = data; });
