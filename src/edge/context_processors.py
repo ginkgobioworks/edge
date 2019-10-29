418682beb78671ce38ebab28b94ffe8a4049f97d
Yaoyu Yang <yaoyu@ginkgobioworks.com>
2019-10-29 15:29:54 -0400

    Make the FULLSTORY_URL an env

--- src/edge/context_processors.py
+++ src/edge/context_processors.py
@@ -4,4 +4,5 @@ import os
 def export_envs(request):
     data = {}
     data['FULLSTORY_ORG_ID'] = os.getenv('FULLSTORY_ORG_ID', '')
+    data['FULLSTORY_URL'] = os.getenv('FULLSTORY_URL', 'fullstory.com')
     return data
