ee921bdcc2760e8518775a3a440e26107fa541bf
Curious Default <devs@ginkgobioworks.com>
2019-10-31 15:05:56 +0000

    Bump version: 2.1.0 → 2.1.1

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ class SdistCommandWithJS(sdist):
 
 setup(
     name='edge-genome',
-    version='2.1.0',
+    version='2.1.1',
 
     author='Ginkgo Bioworks',
     author_email='devs@ginkgobioworks.com',
