b1c0a40409ba869d3d6ed3887e68ca8f3aa5fb19
Benjie Chen <benjie@ginkgobioworks.com>
2020-03-14 11:22:08 -0400

    add comment w/r/t how phase field is handled in qualifiers

--- src/edge/io.py
+++ src/edge/io.py
@@ -40,10 +40,18 @@ class IO(object):
                 # FeatureLocation first bp is AfterPosition, so -1
                 loc = FeatureLocation(annotation.base_first - 1, annotation.base_last)
                 qualifiers = annotation.feature.qualifiers
+
+		# "phase" is how GFF annotations frame shifts. CDS with phase 1
+		# means annotation is 1 bp frameshift. GFF parser we use parses
+		# GFF features into BioPython SeqFeature and stores the "phase"
+		# in the qualifiers. Here we guard against manual annotation
+		# that set phase to None and change it to 0 instead, because it
+		# will be output as None otherwise.
                 if "phase" in qualifiers and qualifiers["phase"] is None:
                     qualifiers["phase"] = 0
                 elif "Phase" in qualifiers and qualifiers["Phase"] is None:
                     qualifiers["Phase"] = 0
+
                 qualifiers.update({'name': annotation.feature_name})
                 strand = annotation.feature.strand
                 feature = SeqFeature(loc,
