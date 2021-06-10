2a08c47ff028f26cd19b313718510c201b5805f9
Curious Default <devs@ginkgobioworks.com>
2021-06-10 01:23:32 +0000

    Bump version: 3.0.1 → 3.1.0

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ class SdistCommandWithJS(sdist):
 
 setup(
     name="edge-genome",
-    version="3.0.1",
+    version="3.1.0",
     author="Ginkgo Bioworks",
     author_email="devs@ginkgobioworks.com",
     url="https://github.com/ginkgobioworks/edge/",
--- src/edge/__init__.py
+++ src/edge/__init__.py
@@ -1,6 +1,6 @@
 from django.db.backends.signals import connection_created
 
-__version__ = "3.0.1"
+__version__ = "3.1.0"
 
 
 import random
