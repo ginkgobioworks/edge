d7c35749c682cb86356cdf825f3886e22b07942a
Patrick Murray <pmurray@ginkgobioworks.com>
2021-10-05 23:51:23 -0400

    Add --refresh command line argument to Django admin command build_genome_blastdb

--- src/edge/management/commands/build_genome_blastdb.py
+++ src/edge/management/commands/build_genome_blastdb.py
@@ -3,5 +3,15 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--refresh',
+            action='store_true',
+            help='Rebuild BLAST database files',
+        )
+
     def handle(self, *args, **options):
-        build_all_genome_dbs()
+        if options['refresh']:
+            build_all_genome_dbs(refresh=True)
+        else:
+            build_all_genome_dbs(refresh=False)
