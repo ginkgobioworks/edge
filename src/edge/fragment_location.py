9cbba2187e137432dc859974d9767b91b7937360
Alec Ethell <aethell@ginkgobioworks.com>
2022-03-16 17:34:36 -0400

    created baselining for fl files

--- src/edge/fragment_location.py
+++ src/edge/fragment_location.py
@@ -1,4 +1,6 @@
 import pickle
+import os, time
+import pandas as pd
 from sortedcontainers import SortedDict
 
 from edge.models.chunk import Chunk
@@ -130,3 +132,57 @@ def reconstruct_fragment_from_fl_file(fn):
         sequences_list.append(chunk.get_sequence())
 
     return ''.join(sequences_list)
+
+def run_baselines_on_gtf(fn):
+    def estimate_size_by_contig(contig, fn):
+        start_time = time.time()
+        column_names = ['contig', 'annotation_source', 'feature_type', 'start', 'end', 'score', 'strand', 'phase', 'extra']
+        df = pd.read_csv("../../hg38.knownGene.gtf", delimiter='\t', header=None, names=column_names)
+        df = df[df['contig'] == contig]
+        print("--------------------------")
+        print(f"Number of {contig} associated rows:", len(df.index))
+
+        pivots = set(pd.unique(df[['start', 'end']].values.ravel('K')))
+        pivots.add(1)
+        n_chunks = len(pivots)
+        print("Number of chunks:", n_chunks, "\n")
+
+        construction_start_time = time.time()
+        coord_to_chunk_id = SortedDict()
+        chunk_id_to_coord = {}
+        for start_coord in pivots:
+            coord_to_chunk_id[start_coord] = start_coord + 1
+            chunk_id_to_coord[start_coord + 1] = start_coord + 1
+
+        fn = "%s/%s" % (".", "test")
+        pickle_start_time = time.time()
+        pickle_fl_file(fn, coord_to_chunk_id, chunk_id_to_coord)
+        print("Time to pandas parse:", construction_start_time - start_time, "seconds")
+        print("Time to construct dictionaries:", pickle_start_time - construction_start_time, "seconds")
+        print("Time to pickle:", time.time() - pickle_start_time, "seconds")
+        print("Time for all:", time.time() - start_time, "seconds")
+        print("Time per chunk", (time.time() - start_time) / n_chunks, "seconds\n")
+        
+        file_size = os.path.getsize(fn)
+
+        print("Size of file:", file_size / (10**6), "MB")
+        print("Bytes per chunk:", file_size / n_chunks)
+
+        return n_chunks, file_size
+
+    total_start_time = time.time()
+    total_chunks = 0
+    total_bytes = 0
+    for i in range(1, 23):
+        contig = f"chr{i}"
+        new_chunks, new_bytes = estimate_size_by_contig(contig, fn)
+        total_chunks += new_chunks
+        total_bytes += new_bytes
+
+    print("--------------------------")
+    print("Over 22 chromosomes:")
+    print("Total chunks:", total_chunks)
+    print("Total bytes:", total_bytes)
+    print("Total bytes per chunk", total_bytes / total_chunks)
+    print("Total time:", time.time() - total_start_time, "seconds")
+    print("Total time per chunk", (time.time() - total_start_time) / total_chunks, "seconds")
