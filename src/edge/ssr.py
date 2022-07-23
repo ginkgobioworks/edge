92c1283ef6c69ec62739b63e58c3943b4cfcf4b3
Benjie Chen <benjie@ginkgobioworks.com>
2022-07-23 08:49:36 -0400

    a bit more progress

--- src/edge/ssr.py
+++ src/edge/ssr.py
@@ -47,22 +47,32 @@ class Sites(object):
     Bxb1_attP = _c("GTCGTGGTTTGTCTGGTCAACCACC gcggtctc AGTGGTGTACGGTACAAACCCCGAC")
 
 
-class SiteLocation(objects):
+class SiteLocation(object):
 
-    def __init__(self, site, fragment, start, end, direction):
+    def __init__(self, site, fragment, insert, start, end, direction):
         self.site = site
         self.fragment = fragment
+        self.insert = insert
         self.start = start
         self.end = end
         self.direction = direction
 
-        # after conversion from start/end to chunks
+        # after conversion from bp coordinates to chunks
         self.prev_chunk = None
         self.first_chunk = None
         self.last_chunk = None
         self.next_chunk = None
 
-        XXX define for both fragment and payload
+    @property
+    def fragment_id(self):
+        return self.fragment.id if self.fragment is not None else None
+
+    @property
+    def on_insert(self):
+        return self.insert is not None
+
+    def chunkify(self):
+        XXX
 
 
 class Recombination(object):
@@ -70,8 +80,7 @@ class Recombination(object):
     Generic class to model a recombination event.
     """
 
-    def possible_locations(self):
-        XXX
+    def possible_locations(self, all_locations):
         XXX also disambiguity
 
     def generate_events(self, locations, event_cls):
@@ -161,38 +170,45 @@ class Event(object):
 
     @property
     def fragment_id(self):
-        XXX
+        for loc in self.locations:
+            if loc.fragment_id:
+                return loc.fragment_id
+        assert False
+
+    def prepare(self):
+        for loc in self.locations:
+            loc.chunkify()
 
 
 class IntegrationEvent(Event):
 
-    def run(self, new_fragment, payload, is_payload_circular):
-        payload = self.rotate_payload()
-        assert payload.index(recombination.site_insert) == 0
+    def run(self, new_fragment, insert, is_insert_circular):
+        insert = self.rotate_insert()
+        assert insert.index(recombination.site_insert) == 0
 
         XXX
         remove old site?
-        create new chunk with payload
+        create new chunk with insert
         flank with two sites
 
 
 class ExcisionEvent(Event):
 
-    def run(self, new_fragment, payload, is_payload_circular):
+    def run(self, new_fragment, insert, is_insert_circular):
         # TODO
         pass
 
 
 class InversionEvent(Event):
 
-    def run(self, new_fragment, payload, is_payload_circular):
+    def run(self, new_fragment, insert, is_insert_circular):
         # TODO
         pass
 
 
 class RMCEEvent(Event):
 
-    def run(self, new_fragment, payload, is_payload_circular):
+    def run(self, new_fragment, insert, is_insert_circular):
         # TODO
         pass
 
@@ -203,32 +219,62 @@ class Reaction(object):
     def allowed():
         return []
 
-    def __init__(self, parent_genome, payload, is_payload_circular):
+    def __init__(self, parent_genome, insert, is_insert_circular):
         self.parent_genome = parent_genome
-        self.payload = payload
-        self.is_payload_circular = is_payload_circular
+        self.insert = insert
+        self.is_insert_circular = is_insert_circular
         self.locations = {}
         self.events = None
         self.errors = []
 
-    def __ensure_locations(self):
+    @classmethod
+    def __sites_on_genome(cls):
+        sites = []
+        for recombination in cls.allowed():
+            sites.extend(recombination.required_genome_sites())
+        return set(sites)
+
+    @classmethod
+    def __sites_on_insert(cls):
+        sites = []
+        for recombination in cls.allowed():
+            sites.extend(recombination.required_insert_sites())
+        return set(sites)
+
+    @staticmethod
+    def __add_reverse_sites(sites):
+        new_sites = []
+        for s in sites:
+            new_sites.append(s)
+            new_sites.append(rc(s))
+        return set(new_sites)
+
+    def determine_site_locations(self):
+	# FIXME this is a very slow implementation, we can make this faster by
+	# using blast (i think)
+
+        if self.insert:
+            sites = self.__add_reverse_sites(self.__sites_on_insert())
+            template = self.insert
+            if self.is_insert_circular:
+                template = self.insert*2
+            template = template.lower()
+            XXX
+
         XXX
+
 
     def group_into_events(self):
 	# TODO if we attempt an integration with loxP on loxP, how do we return
 	# that as an error not just silently ignore?
 
-        self.__ensure_locations(self)
+        self.determine_site_locations(self)
         self.events = []
 
         for recombination in self.allowed():
             self.events.extend(recombination.events(self.locations))
 
     def run_reaction(self):
-        self.__ensure_locations()
-        if self.errors:
-            return
-
         self.group_into_events()
         if self.errors:
             return
@@ -241,12 +287,21 @@ class Reaction(object):
         new_genome.save()
 
         old_to_new_fragment_dict = {}
+
+	# first prepare all events for execution, then execute; this allows
+	# events to convert bp coordinates to chunk objects, so we are not
+	# depending on bp coordinates from before making any changes
+
         for event in self.events:
             if event.fragment_id not in old_to_new_fragment_dict:
                 with new_genome.update_fragment_by_fragment_id(
                     event.fragment_id, new_fragment=True
                 ) as f:
                     old_to_new_fragment_dict[event.fragment_id] = f
+            new_fragment = old_to_new_fragment_dict[event.fragment_id]
+            event.prepare(new_fragment)
+
+        for event in self.events:
             new_fragment = old_to_new_fragment_dict[event.fragment_id]
             event.run(new_fragment)
         
