96d0c95a0db1347ef49e36a35864d85215c37ffe
Alec Ethell <aethell@ginkgobioworks.com>
2022-09-09 11:37:21 -0400

    bumped edge-genome version to 3.27.1

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ class SdistCommandWithJS(sdist):
 
 setup(
     name="edge-genome",
-    version="3.27.0",
+    version="3.27.1",
     author="Ginkgo Bioworks",
     author_email="devs@ginkgobioworks.com",
     url="https://github.com/ginkgobioworks/edge/",
--- src/edge/__init__.py
+++ src/edge/__init__.py
@@ -1,6 +1,6 @@
 from django.db.backends.signals import connection_created
 
-__version__ = "3.27.0"
+__version__ = "3.27.1"
 
 
 import random
